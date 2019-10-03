d5ac537e0c53abffc06695054887c30876eafd8f
Zbigniew Kuzera <zbigniew.kuzera@allegro.pl>
2019-10-03 10:45:59 +0200

    first commit

---
+++ build.gradle.kts
@@ -0,0 +1,43 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+	id("org.springframework.boot") version "2.2.0.M6"
+	id("io.spring.dependency-management") version "1.0.8.RELEASE"
+	kotlin("jvm") version "1.3.50"
+	kotlin("plugin.spring") version "1.3.50"
+}
+
+group = "com.example"
+version = "0.0.1-SNAPSHOT"
+java.sourceCompatibility = JavaVersion.VERSION_11
+
+repositories {
+	mavenCentral()
+	maven { url = uri("https://repo.spring.io/milestone") }
+	maven { url = uri("https://repo.spring.io/snapshot") }
+}
+
+dependencies {
+	implementation("org.springframework.boot:spring-boot-starter-webflux")
+	implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
+	implementation("org.jetbrains.kotlin:kotlin-reflect")
+	implementation("org.jetbrains.kotlin:kotlin-stdlib")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core")
+	implementation("org.jetbrains.kotlinx:kotlinx-coroutines-reactor")
+	testImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
+		exclude(group = "junit", module = "junit")
+	}
+	testImplementation("io.projectreactor:reactor-test")
+}
+
+tasks.withType<Test> {
+	useJUnitPlatform()
+}
+
+tasks.withType<KotlinCompile> {
+	kotlinOptions {
+		freeCompilerArgs = listOf("-Xjsr305=strict")
+		jvmTarget = "1.8"
+	}
+}
---
+++ src/main/kotlin/com/example/Controller.kt
@@ -0,0 +1,76 @@
+package com.example
+
+import kotlinx.coroutines.delay
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+
+@Controller
+@RequestMapping("/")
+@SpringBootApplication
+class Controller {
+	private val logger: Logger = LoggerFactory.getLogger("controller")
+
+	@GetMapping("/ingredients") @ResponseBody
+	suspend fun ingredients(@RequestParam(value = "value") value: String): String? {
+        val delayMs = delayRandomMs()
+		val returnedValue = if (listOf("flour", "cinammon", "soda", "sugar", "egg").containsAll(value.split(",")))
+			"ok"
+		else "not ok"
+		return returnedValue.also { logger.info("GET /ingredients?value=$value after $delayMs delay returns $it ") }
+	}
+
+	@GetMapping("/heat") @ResponseBody
+	suspend fun heat(@RequestParam(value = "value") value: String): String? {
+        val delayMs = delayRandomMs()
+        val returnedValue = if (listOf("butter", "honey").containsAll(value.split(",")))
+			"ok"
+		else "not ok"
+        return returnedValue.also { logger.info("GET /heat?value=$value after $delayMs delay returns $it ") }
+	}
+
+	@GetMapping("/dough") @ResponseBody
+	suspend fun dough(@RequestParam(value = "value") value: String): String? {
+        val delayMs = delayRandomMs()
+        val returnedValue = if (listOf("flour", "cinammon", "soda", "sugar").containsAll(value.split(",")))
+			"ok"
+		else "not ok"
+        return returnedValue.also { logger.info("GET /dough?value=$value after $delayMs delay returns $it ") }
+	}
+
+	@GetMapping("/tray") @ResponseBody
+	suspend fun tray(): String? {
+        val delayMs = delayRandomMs()
+        val returnedValue = if ((0..100).random() > 30)
+			"ok"
+		else "not ok"
+        return returnedValue.also { logger.info("GET /tray after $delayMs delay returns $it ") }
+	}
+
+	@GetMapping("/icing") @ResponseBody
+	suspend fun icing(): String? {
+        val delayMs = delayRandomMs()
+        val returnedValue = if ((0..100).random() > 30)
+			"ok"
+		else "not ok"
+        return returnedValue.also { logger.info("GET /icing after $delayMs delay returns $it ") }
+	}
+
+    private suspend fun delayRandomMs(): Long {
+        val randomTimeMillis = (0..50L).random()
+        delay(randomTimeMillis)
+        return randomTimeMillis
+    }
+}
+
+fun main(args: Array<String>) {
+	runApplication<com.example.Controller>(*args)
+}
+
+
